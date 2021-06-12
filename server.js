4bea290a6e390bcf0810f7557fa8350670f46d7c
dustingottlieb <dustingottlieb@gmail.com>
2021-06-12 13:19:08 -0500

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/dustins-fitness-tracker', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
